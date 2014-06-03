1dde0ec181da1e3ee40eb7c040d63792552d8a8c
azyobuzin <azyobuzin@users.sourceforge.jp>
2014-06-03 22:16:06 +0900

    Initial commit

---
+++ Cea/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Diagnostics;
+using javassist;
+
+namespace Cea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cp = ClassPool.getDefault();
+            var cc = cp.makeClass("test.MyClass");
+            cc.addField(CtField.make("int num;", cc));
+            var bytes = cc.toBytecode();
+            Debugger.Break();
+        }
+    }
+}
